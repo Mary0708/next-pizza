47c67047e4b9e271a5961417f1dde5da79c1a619
unknown <mary.liss133@gmail.com>
2024-09-04 20:15:26 +0500

    init: создали проект, добавили шапку

---
+++ next-pizza/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@/components/shared";
+import { Title } from "@/components/shared/title";
+
+export default function Home() {
+  return (
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+    </>
+  );
+}
---
+++ next-pizza/src/components/ui/index.ts
@@ -0,0 +1,9 @@
+export { Button } from "./button";
+export { Skeleton } from "./skeleton";
+export { Checkbox } from "./checkbox";
+export { Dialog } from "./dialog";
+export { Drawer } from "./drawer";
+export { Input } from "./input";
+export { Select } from "./select";
+export { Popover } from "./popover";
+export { Slider } from "./slider";
